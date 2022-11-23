ed7bfa2fbae2bafb558649b1452a2ec495b57c50
susannaclark <susannaclark95@gmail.com>
2022-11-23 10:11:30 +0000

    put comments/ plan in results page and its child components

---
+++ src/components/OtherPeople/OtherPeople.js
@@ -0,0 +1,6 @@
+// this component will need a drop down box to choose who you played against - the list of other players will be passed in here as props from Results
+
+// this componened needs a header with the other player's nickname in it (passed in from other player's state in Results as props)
+// this component also needs to show some kind of avatar image underneath the header
+// under that this component will show all of the answers of the other player (passed in from other player's state in Results as props)
+//both the header and the answers will use the same state from Results so will access parts of it with dot notation
