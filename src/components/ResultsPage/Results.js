2497c2049fae0e7c38c7ce4c956e8393d78a54bc
susannaclark <susannaclark95@gmail.com>
2022-11-23 10:32:01 +0000

    completed matches planning

--- src/components/ResultsPage/Results.js
+++
@@ -1,9 +0,0 @@
-//this component will need to handle 3 states:
-//one for storing the main person's answers after they have been GET requested within useEffect
-//one for storing the list of other player's so far from the table (again GET request within useEffect) 
-//the above will enable the user to select who they played against
-//and third state will be for the selected player's answers
-
-//Main person component will be rendered here with the main person's answers state dropped in as props and the nickname state from APP dropped in as props
-// OtherPeople component will be rendered here with the list of other players state passed in, and then with a function passed into it that is written within Results which will mean when a player is selected from Other People's drop down list a GET request for their answers and nickname is sent, the result of the get request will also need to be passed in as props
-//Matches component will be rendered here with the data from both people's answers and some kind of function to get a match percentage
