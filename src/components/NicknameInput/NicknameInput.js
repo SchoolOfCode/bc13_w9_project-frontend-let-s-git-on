eadd9d7862c8af48fcf356010db0abed9071f5dc
BenPuusta <benpuusta@gmail.com>
2022-11-23 14:52:34 +0000

    started testing backend functionality

--- src/components/NicknameInput/NicknameInput.js
+++ src/components/NicknameInput/NicknameInput.js
@@ -2,11 +2,11 @@ import React from "react";
 import './NicknameInput.css';
 
 
-export default function NicknameInput({userNickname, handleNickname}) {
+export default function NicknameInput({userNickname, handleNickname, testProp}) {
     //user name input page - this will be the first page to display in our app
  return (
     <div>
-        <h2 className="user-input">😸What's your username?</h2>
+        <h2 className="user-input">😸What's your username? {testProp}</h2>
         <input id='nicknameInput' placeholder="write username here..." type='text'></input>
         <button className="btn" onClick={handleNickname}>Next</button>
         <p>{userNickname}</p>
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import NicknameInput from '../NicknameInput/NicknameInput';
 import SelectionMenu from '../SelectionMenu/SelectionMenu';
 import Question1 from '../Question/Question1';
@@ -8,16 +8,48 @@ import Question3 from '../Question/Question3';
 import Question4 from '../Question/Question4';
 import Question5 from '../Question/Question5';
 
+// async function testPlayers() {
+// const response = await fetch("http://localhost:3000/api/players"
+// )
+// const data = await response.json();
+// console.log (data)
+// }
+//  testPlayers()
+//here to test fetching at the most basic level
+
+
 
 function App() {
   // all the states we are using so far
   const [nickname, setNickname] = useState('');
   const [selection, setSelection] = useState('');
   const [question, setQuestion] = useState(1);
-  
+
   //pass in props and create a question tag with the text e.g <question text="question here?"/>
   //create a new state here for the questions 
   //Create five different objects with the questions and options
+
+  const [gotdata, setData] = useState('');
+// testing out state functionality interacting with fetched API data
+
+
+useEffect(() => {
+  //Runs only on the first render
+  async function getPlayers() {
+
+    const response = await fetch("http://localhost:3000/api/players"
+    )
+    const data = await response.json();
+  setData (data)
+  console.log (gotdata)
+    } 
+    getPlayers()
+
+}, []);
+
+
+
+
 
   
   // function for when you click on next after typing in a nickname
@@ -49,7 +81,7 @@ function App() {
   if (nickname.length === 0) {
   return (
     <div className="App">
-      <NicknameInput userNickname={nickname} handleNickname={handleNickname}/>
+      <NicknameInput userNickname={nickname} handleNickname={handleNickname} testProp={''}/>
     </div>
   );
   }
