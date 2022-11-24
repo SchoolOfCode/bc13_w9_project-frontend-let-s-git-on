097b5d3e86965bd5056610f1a4249ebf8299575a
BenPuusta <benpuusta@gmail.com>
2022-11-24 10:57:18 +0000

    more state

--- src/components/ResultsPage/ResultsPage.js
+++ src/components/ResultsPage/ResultsPage.js
@@ -7,7 +7,7 @@
 //Main person component will be rendered here with the main person's answers state dropped in as props and the nickname state from APP dropped in as props
 // SecondPerson component will be rendered here with the list of other players state passed in, and then with a function passed into it that is written within Results which will mean when a player is selected from Second People's drop down list a GET request for their answers and nickname is sent, the result of the get request will also need to be passed in as props
 // Matches component will be rendered here with the data from both people's answers and some kind of function to get a match percentage
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import React from "react";
 import MainPerson from "../MainPerson/MainPerson";
 
@@ -15,20 +15,30 @@ import MainPerson from "../MainPerson/MainPerson";
 
 
 
-export default function ResultsPage({userNickname, userNumber}) {
-    const [userState, setUserState] = useState('')
 
-    async function getStats (id) {
+
+export default function ResultsPage({userNickname, userNumber}) {
+    const [userState, setUserState] = useState(getStats())
+
+     async function getStats (id) {
         const response = await fetch (`http://localhost:3000/api/players/${userNumber}`)
         let unpackedJson = await response.json()
-        console.log ('PLAYERS STATS:', unpackedJson)
-        setUserState(unpackedJson)
+        console.log ('PLAYERS STATS:', unpackedJson.payload)
+        // setUserState(unpackedJson.payload)
+        // console.log ('userState:', userState)
+        return unpackedJson.payload
         }
-        
-getStats()
+console.log (userState)
+// useEffect(() => {
+//     // Run! Like go get some data from an API.
+//     setUserState(getStats())
+//     console.log ('USERSTATE', userState)
+//   }, []);
+
     //user name input page - this will be the first page to display in our app
  return (
     <div>
+    <div>{userState[0]}</div>
         <MainPerson userNickname={userNickname} userNumber={userNumber}/>
         <p>Your personal ID is {userNumber}, anyone you play with will need to know this number to compare your results.</p>
         
