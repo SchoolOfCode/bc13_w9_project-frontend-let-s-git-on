9d68d25fdb2ddc29b0f9297002c284ae38171bac
susannaclark <susannaclark95@gmail.com>
2022-11-24 15:47:47 +0000

    no real changes but I indented a few things and don't want a merge conflict

--- src/components/ResultsPage/ResultsPage.js
+++ src/components/ResultsPage/ResultsPage.js
@@ -1,12 +1,3 @@
-//this component will need to handle 3 states:
-//one for storing the main person's answers after they have been GET requested within useEffect
-//one for storing the list of second player's so far from the table (again GET request within useEffect) 
-//the above will enable the user to select who they played against
-//and third state will be for the seecond player's answers
-
-//Main person component will be rendered here with the main person's answers state dropped in as props and the nickname state from APP dropped in as props
-// SecondPerson component will be rendered here with the list of other players state passed in, and then with a function passed into it that is written within Results which will mean when a player is selected from Second People's drop down list a GET request for their answers and nickname is sent, the result of the get request will also need to be passed in as props
-// Matches component will be rendered here with the data from both people's answers and some kind of function to get a match percentage
 import {useState, useEffect} from "react"
 import React from "react";
 //import MainPerson from "../MainPerson/MainPerson";
@@ -34,14 +25,14 @@ useEffect(() => {
 
   async function sendP2(value) {
     const response = await fetch (`http://localhost:3000/api/players/${value}`)
-let unpackedJson = await response.json()
+    let unpackedJson = await response.json()
     console.log (unpackedJson.payload)
     setP2State(unpackedJson.payload)
   }
 
   function handleP2(){
     let value = document.getElementById('p2Input').value
-sendP2(value)
+    sendP2(value)
     } 
   
 
